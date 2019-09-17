d93d09ac75a3f5614dc383a329e3f9ae8b6a6a52
THE_DMA <duashmaht@hotmail.com>
2019-09-16 20:56:52 -0400

    Update and rename Unique Characters to UniqueCharacters.cpp

--- UniqueCharacters.cpp
+++ UniqueCharacters.cpp
@@ -1,10 +1,16 @@
 
-
-
+#include <string>
+using namespace std;
 
 string isUnique ( string s ) {
    vector<int> v ( 128 , 0);
    for (auto c : s ) if (isalpha(c)) v[c]++;
    for (auto c : s ) if (isalpha(c)  && v[c] > 1) return "No";
    return "Yes";
+}
+
+int main () {
+   cout << isUnique("duale") << endl;
+    cout << isUnique("dualae") << endl;
+     cout << isUnique("dualeaaaa") << endl;
 }
